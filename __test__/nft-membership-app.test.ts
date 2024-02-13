c7741b24e71c43316e2f59ad0624c0c43a0e3b7e
iskysun96 <iskysun96@gmail.com>
2024-02-12 18:39:09 -0800

    first commit

--- __test__/nft-membership-app.test.ts
+++ __test__/nft-membership-app.test.ts
@@ -1,17 +1,29 @@
 import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
 import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
+import algosdk from 'algosdk';
+import * as algokit from '@algorandfoundation/algokit-utils';
+import { AlgoAmount } from '@algorandfoundation/algokit-utils/types/amount';
 import { NftMembershipAppClient } from '../contracts/clients/NftMembershipAppClient';
 
 const fixture = algorandFixture();
 
 let appClient: NftMembershipAppClient;
+let appAddr: string;
+let appId: number | bigint;
+let algod: algosdk.Algodv2;
+let testAccount: algosdk.Account;
+const BOX_COST = 2500 + 400 * (1 + 32);
 
 describe('NftMembershipApp', () => {
   beforeEach(fixture.beforeEach);
+  // beforeEach(() => {
+  //   sharedState
+  // })
 
   beforeAll(async () => {
     await fixture.beforeEach();
-    const { algod, testAccount } = fixture.context;
+    algod = fixture.context.algod;
+    testAccount = fixture.context.testAccount;
 
     appClient = new NftMembershipAppClient(
       {
@@ -22,20 +34,87 @@ describe('NftMembershipApp', () => {
       algod
     );
 
-    await appClient.create.createApplication({});
+    const appInfo = await appClient.create.createApplication({ membershipPrice: 1000000 });
+
+    appAddr = appInfo.appAddress;
+    appId = appInfo.appId;
+
+    algokit.transferAlgos(
+      {
+        amount: algokit.algos(0.1),
+        from: testAccount,
+        to: appAddr,
+      },
+      algod
+    );
+
+    const globalState = await appClient.getGlobalState();
+    expect(globalState.membershipPrice?.asNumber()).toBe(1000000);
   });
 
-  test('sum', async () => {
-    const a = 13;
-    const b = 37;
-    const sum = await appClient.doMath({ a, b, operation: 'sum' });
-    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
+  // create nft
+  test('Create a membership NFT', async () => {
+    const name = 'Membership NFT';
+    const total = 1000;
+    const unitName = 'MEM';
+    const assetUrl = 'https://example.com/membership-nft';
+
+    // Conver minimum balance for asset creation
+    algokit.transferAlgos(
+      {
+        amount: algokit.algos(0.1),
+        from: testAccount,
+        to: appAddr,
+      },
+      algod
+    );
+
+    const createdAsset = await appClient.createMembershipNft(
+      {
+        name,
+        total,
+        unitName,
+        assetUrl,
+      },
+      { sendParams: { fee: algokit.transactionFees(2) } }
+    );
+
+    const globalState = await appClient.getGlobalState();
+    expect(globalState.membershipNft).toBeDefined();
+    expect(createdAsset.return?.valueOf()).toBeDefined();
   });
 
-  test('difference', async () => {
-    const a = 13;
-    const b = 37;
-    const diff = await appClient.doMath({ a, b, operation: 'difference' });
-    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
+  // get membership
+  test('Get membership', async () => {
+    const sp = await algod.getTransactionParams().do();
+    const paymentTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      from: testAccount.addr,
+      to: appAddr,
+      suggestedParams: sp,
+      amount: 1000000 + BOX_COST,
+    });
+
+    const globalState = await appClient.getGlobalState();
+    const membershipNftId = globalState.membershipNft?.asNumber();
+    console.log('membership nft id ', membershipNftId);
+
+    const optIntoNft = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+      from: testAccount.addr,
+      to: testAccount.addr,
+      suggestedParams: sp,
+      assetIndex: Number(membershipNftId),
+      amount: 0,
+    });
+
+    await appClient.getMembership(
+      { payment: paymentTxn, nftOptIn: optIntoNft },
+      { sendParams: { fee: algokit.transactionFees(2) } }
+    );
+    const memberInfo = await appClient.appClient.getBoxValue(testAccount.addr);
+    const accountInfo = await algod.accountInformation(testAccount.addr).do();
+    console.log(accountInfo);
+    expect(memberInfo).toBe(true);
   });
+
+  // cancel membership
 });
--- contracts/nft-membership-app.algo.ts
+++ contracts/nft-membership-app.algo.ts
@@ -1,47 +1,105 @@
 import { Contract } from '@algorandfoundation/tealscript';
 
+const BOX_COST = 2500 + 400 * (1 + 32);
+
 // eslint-disable-next-line no-unused-vars
 class NftMembershipApp extends Contract {
-  /**
-   * Calculates the sum of two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The sum of a and b
-   */
-  private getSum(a: number, b: number): number {
-    return a + b;
+  membershipPrice = GlobalStateKey<uint64>();
+
+  membershipNft = GlobalStateKey<Asset>();
+
+  memberInfo = BoxMap<Address, boolean>();
+
+  authorizeCreator(): void {
+    assert(this.app.creator === this.txn.sender);
+  }
+
+  createApplication(membershipPrice: uint64): void {
+    this.membershipPrice.value = membershipPrice;
   }
 
   /**
-   * Calculates the difference between two numbers
+   * A method that creates the Membership NFT
    *
-   * @param a
-   * @param b
-   * @returns The difference between a and b.
+   * TODO: should the asset total be a power of 10? and should the decimals be 0?
+   *
+   * @param name
+   * @param total
+   * @param unitName
+   * @param assetUrl
    */
-  private getDifference(a: number, b: number): number {
-    return a >= b ? a - b : b - a;
+  createMembershipNft(name: string, total: uint64, unitName: string, assetUrl: string): Asset {
+    this.authorizeCreator();
+    // Create a new NFT
+    const createdAsset = sendAssetCreation({
+      configAssetName: name,
+      configAssetTotal: total,
+      configAssetDecimals: 0,
+      configAssetUnitName: unitName,
+      configAssetURL: assetUrl,
+      configAssetClawback: this.app.address,
+      configAssetDefaultFrozen: 1,
+      configAssetManager: this.app.address,
+      configAssetReserve: this.app.address,
+      configAssetFreeze: this.app.address,
+      fee: 0,
+    });
+
+    this.membershipNft.value = createdAsset;
+
+    return createdAsset;
   }
 
   /**
-   * A method that takes two numbers and does either addition or subtraction
+   * A method that allows a user to purchase a membership
    *
-   * @param a The first number
-   * @param b The second number
-   * @param operation The operation to perform. Can be either 'sum' or 'difference'
-   *
-   * @returns The result of the operation
+   * @param payment
    */
-  doMath(a: number, b: number, operation: string): number {
-    let result: number;
+  getMembership(payment: PayTxn, nftOptIn: AssetTransferTxn): void {
+    assert(this.membershipNft.exists);
+    assert(payment.amount === this.membershipPrice.value + BOX_COST);
+    assert(payment.receiver === this.app.address);
+    assert(payment.sender === this.txn.sender);
+    // assert(nftOptIn.assetAmount === 0);
+    // assert(nftOptIn.assetReceiver === nftOptIn.assetSender);
+    // assert(nftOptIn.xferAsset.id === this.membershipNft.value.id);
 
-    if (operation === 'sum') {
-      result = this.getSum(a, b);
-    } else if (operation === 'difference') {
-      result = this.getDifference(a, b);
-    } else throw Error('Invalid operation');
+    sendAssetTransfer({
+      xferAsset: this.membershipNft.value,
+      assetAmount: 1,
+      assetSender: this.app.address,
+      assetReceiver: this.txn.sender,
+      fee: 0,
+    });
 
-    return result;
+    this.memberInfo(this.txn.sender).value = true;
+  }
+
+  /**
+   * A method that allows a user to cancel their membership
+   */
+  cancelMembership(): void {
+    assert(this.memberInfo(this.txn.sender).value === true);
+    assert(this.membershipNft.exists);
+
+    // Clawback asset transfer to retrieve the membership NFT
+    sendAssetTransfer({
+      sender: this.app.address,
+      xferAsset: this.membershipNft.value,
+      assetAmount: 1,
+      assetSender: this.txn.sender,
+      assetReceiver: this.app.address,
+      fee: 0,
+    });
+
+    this.memberInfo(this.txn.sender).delete();
+
+    const refundAmount = this.membershipPrice.value + BOX_COST;
+
+    sendPayment({
+      amount: refundAmount,
+      receiver: this.txn.sender,
+      fee: 0,
+    });
   }
 }
