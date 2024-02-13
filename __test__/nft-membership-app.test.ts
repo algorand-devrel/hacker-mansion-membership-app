a6d720a538e840caf7732f4a8562b2cbe3c2a008
iskysun96 <iskysun96@gmail.com>
2024-02-12 16:28:27 -0800

    Project initialised with AlgoKit CLI using template: https://github.com/algorand-devrel/tealscript-algokit-template.git

---
+++ __test__/nft-membership-app.test.ts
@@ -0,0 +1,41 @@
+import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
+import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
+import { NftMembershipAppClient } from '../contracts/clients/NftMembershipAppClient';
+
+const fixture = algorandFixture();
+
+let appClient: NftMembershipAppClient;
+
+describe('NftMembershipApp', () => {
+  beforeEach(fixture.beforeEach);
+
+  beforeAll(async () => {
+    await fixture.beforeEach();
+    const { algod, testAccount } = fixture.context;
+
+    appClient = new NftMembershipAppClient(
+      {
+        sender: testAccount,
+        resolveBy: 'id',
+        id: 0,
+      },
+      algod
+    );
+
+    await appClient.create.createApplication({});
+  });
+
+  test('sum', async () => {
+    const a = 13;
+    const b = 37;
+    const sum = await appClient.doMath({ a, b, operation: 'sum' });
+    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
+  });
+
+  test('difference', async () => {
+    const a = 13;
+    const b = 37;
+    const diff = await appClient.doMath({ a, b, operation: 'difference' });
+    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
+  });
+});
